904cfcdd50c25d1de76bcb545579276c31d06afd
Cristian Rojas <injcristianrojas@gmail.com>
2021-05-10 16:05:05 -0400

    Now works as login test

--- src/main/java/cl/injcristianrojas/as400/MainClass.java
+++ src/main/java/cl/injcristianrojas/as400/MainClass.java
@@ -16,26 +16,33 @@ public class MainClass {
     public static void main(String[] args) {
         connData = new ConnectionData();
         connectionTest();
-        testJDBC("hmaturana' or '1'='1");
+        testJDBC("hmaturana", "123' or '1'='1");
     }
 
-    private static void testJDBC(String username) {
+    private static void testJDBC(String username, String password) {
+        if (!connectionTest()) {
+            System.out.println("Connection error. Shutting down...");
+            return;
+        }
         try {
             Class.forName("com.ibm.as400.access.AS400JDBCDriver");
             Connection conn = DriverManager.getConnection("jdbc:as400://pub400.com/" + connData.getMainLibrary(), connData.getUsername(), connData.getPassword());
             Statement stmt = conn.createStatement();
-            ResultSet rs;
-            rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + username +"'");
-            while ( rs.next() )
-                System.out.printf("User selected: %s %s%n", rs.getString("name"), rs.getString("surname"));
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + username +"' and password = '" + password + "'");
+            int rows = 0;
+            while ( rs.next() ) {
+                //System.out.printf("User selected: %s %s%n", rs.getString("name"), rs.getString("surname"));
+                rows++;
+            }
             rs.close();
             conn.close();
+            System.out.println(rows > 0 ? "Login successful" : "Username/password incorrect");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private static void connectionTest() {
+    private static boolean connectionTest() {
         boolean successful;
 
         AS400 as400 = new AS400(connData.getHost(), connData.getUsername(), connData.getPassword());
@@ -44,9 +51,10 @@ public class MainClass {
         } catch (AS400SecurityException | IOException e) {
             successful = false;
         }
-        System.out.println(successful ? "Connection OK" : "Connection error");
 
         as400.disconnectAllServices();
+
+        return successful;
     }
 
 }
